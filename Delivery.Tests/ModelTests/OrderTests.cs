a4d4364007f45ebecf402df24f9c1f78a1d7a57a
Jesse Callahan <jessetylercallahan@gmail.com>
2021-05-08 11:47:21 -0700

    order tests look good

--- Delivery.Tests/ModelTests/OrderTests.cs
+++ Delivery.Tests/ModelTests/OrderTests.cs
@@ -6,13 +6,55 @@ using System;
 namespace Delivery.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("test Order Title", 100, "Test Description");
+      Order newOrder = new Order("test Order Title", "Test Description", 100);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllOrderObjects_OrderList()
+    {
+      string title01 = "cafe";
+      string title02 = "corner store";
+      int price01 = 100;
+      int price02 = 200;
+      string description01 = "suburbs";
+      string description02 = "downtown";
+      Order newOrder1 = new Order(title01, description01, price01);
+      Order newOrder2 = new Order(title02, description02, price02);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+
+      List<Order> result = Order.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectOrder_Order()
+    {
+      string title01 = "cafe";
+      string title02 = "corner store";
+      int price01 = 100;
+      int price02 = 200;
+      string description01 = "suburbs";
+      string description02 = "downtown";
+      Order newOrder1 = new Order(title01, description01, price01);
+      Order newOrder2 = new Order(title02, description02, price02);
+
+      Order result = Order.Find(2);
+
+      Assert.AreEqual(newOrder2, result);
+    }
+
   }
 }
