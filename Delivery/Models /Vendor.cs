0e29fedf5e0d2567cf139fed622db11e5b65cc1d
Jesse Callahan <jessetylercallahan@gmail.com>
2021-05-07 22:44:06 -0700

    add models

--- Delivery/Models /Vendor.cs
+++ Delivery/Models /Vendor.cs
@@ -6,15 +6,17 @@ namespace Delivery.Models
   {
     private static List<Vendor> _instances = new List<Vendor> { };
     public string Name { get; set; }
+    public string Description { get; set; }
     public int Id { get; }
     public List<Order> Orders { get; set; }
 
-    public Vendor(string VendorName)
+    public Vendor(string VendorName, string description)
     {
       Name = VendorName;
+      Description = description;
       _instances.Add(this);
       Id = _instances.Count;
-      Items = new List<Item> { };
+      Orders = new List<Order> { };
     }
 
     public static void ClearAll()
